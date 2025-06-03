86d24e72a2cc78277dba37b9211f7879ef7eb5fd
3rigx <greendlyyounglubert@gmail.com>
2025-06-03 16:09:03 +0100

    added projects

--- lib/pages/portfolio_page.dart
+++ lib/pages/portfolio_page.dart
@@ -22,6 +22,136 @@ class _PortfolioPageState extends State<PortfolioPage> {
   final List<Map<String, dynamic>> caseStudies = [
     {
       'id': '01',
+      'title': 'Election Projection System- Built with react - Tableau - SQL',
+      'images': ['assets/election1.png', 'assets/election2.png'],
+      'statistics': [
+        {
+          'percentage': '85%',
+          'description': 'Election Projection\n Succes rate'
+        },
+        {
+          'percentage': '75%',
+          'description': 'Increase in\nward level projections'
+        }
+      ],
+      'tags': [
+        'TABLEAU',
+        'WEB',
+        'SQL',
+        'REACTJS',
+        'DATA ANALYTICS',
+        'ELECTION PROJECTION'
+      ]
+    },
+    {
+      'id': '02',
+      'title': 'Hotel Management System - Built with Flutter',
+      'images': ['assets/hotel1.png', 'assets/hotel2.png'],
+      'statistics': [
+        {
+          'percentage': '32%',
+          'description': 'Increased Revenue \nand Profitability'
+        },
+        {
+          'percentage': '20%',
+          'description': 'Improved Operational \nEfficiency'
+        }
+      ],
+      'tags': ['MOBILE', 'WEB', '500K MAU']
+    },
+    {
+      'id': '03',
+      'title': 'SmartCRM - comprehensive CRM system',
+      'images': ['assets/crm2.jpg', 'assets/crm6.jpg'],
+      'statistics': [
+        {'percentage': '42%', 'description': 'Increase in\nSales Management'},
+        {
+          'percentage': '85%',
+          'description': 'Increase in\nInsights and Analytics'
+        }
+      ],
+      'tags': [
+        'ERP Integration',
+        'WEB',
+        'Data Analytics',
+        'SQL',
+        'Local Server',
+        'ReactJS'
+      ]
+    },
+
+    {
+      'id': '04',
+      'title':
+          'Luminova Trust Capital | Financial Solutions & Investment Services Built with Flutter',
+      'images': ['assets/lumi3.png', 'assets/lumi4.png'],
+      'statistics': [
+        {
+          'percentage': '12%',
+          'description': 'Raised capital\nfrom MassChallenge'
+        },
+        {
+          'percentage': '',
+          'description': 'Business brokerage\nfinancial consulting'
+        }
+      ],
+      'tags': ['MOBILE', 'WEB', 'Financial Solutions', 'Investment Services']
+    },
+    {
+      'id': '05',
+      'title':
+          'OnlyBandOption | Cryptocurrency & Investment Services Built with Flutter',
+      'images': ['assets/onad4.png', 'assets/onad7.png'],
+      'statistics': [
+        {
+          'percentage': '73%',
+          'description': 'Trust Score\nin AI guided trading'
+        },
+        {
+          'percentage': '15%',
+          'description': 'Traffic ranking in\nreturning users'
+        }
+      ],
+      'tags': ['MOBILE', 'WEB', 'Cryptocurrency', 'Investments', 'Flutter']
+    },
+    {
+      'id': '06',
+      'title': 'Investment Cove built with Flutter',
+      'images': ['assets/cove2.png', 'assets/cove6.png'],
+      'statistics': [
+        {
+          'percentage': '8%',
+          'description': 'Foreign Direct Investment\nDecline in 2024'
+        },
+        {
+          'percentage': '5%',
+          'description': 'Increase in\nMutual funds \nand ETFs'
+        }
+      ],
+      'tags': ['MOBILE', 'WEB', 'Investments', 'Flutter']
+    },
+    {
+      'id': '07',
+      'title': 'Royal Express Logistics, Built with FLutter.',
+      'images': ['assets/royalexpress1.png', 'assets/royalexpress3.png'],
+      'statistics': [
+        {
+          'percentage': '25%',
+          'description': 'Increase in\nsuccessful placements'
+        },
+        {'percentage': '15%', 'description': 'Reduction in\ndelivery time'}
+      ],
+      'tags': [
+        'LOGISTICS',
+        'SHIPPING',
+        'ENTERPRISE',
+        'MOBILE',
+        'WEB',
+        'FLUTTER'
+      ]
+    },
+    {
+      'id': '08',
       'title': 'Cosmetic brand Flawless - Created with Wordpress',
       'images': ['assets/flawless3.jpg', 'assets/flawless2.jpeg'],
       'statistics': [
@@ -37,7 +167,7 @@ class _PortfolioPageState extends State<PortfolioPage> {
       'tags': ['MOBILE', 'WEB', '500K MAU']
     },
     {
-      'id': '02',
+      'id': '09',
       'title':
           'logistics company dedicated to fast, secure, and reliable shipping, Built with Wordpress.',
       'images': ['assets/parcelkeepers1.jpeg', 'assets/parcelkeepers2.jpeg'],
@@ -47,110 +177,6 @@ class _PortfolioPageState extends State<PortfolioPage> {
           'description': 'Customer support\nReal-time tracking\nglobal reach'
         },
         {'percentage': '1.9%', 'description': 'Click rates'}
-      ],
-      'tags': ['MOBILE', 'WEB', '500K MAU']
-    },
-    {
-      'id': '03',
-      'title': 'Royal Express Logistics, Built with FLutter.',
-      'images': ['assets/royalexpress1.png', 'assets/royalexpress3.png'],
-      'statistics': [
-        {
-          'percentage': '25%',
-          'description': 'Increase in\nsuccessful placements'
-        },
-        {'percentage': '15%', 'description': 'Reduction in\ndelivery time'}
-      ],
-      'tags': ['LOGISTICS', 'SHIPPING','ENTERPRISE', 'MOBILE', 'WEB', 'FLUTTER']
-    },
-    {
-      'id': '04',
-      'title': 'Luminova Trust Capital | Financial Solutions & Investment Services Built with Flutter',
-      'images': ['assets/lumi3.png', 'assets/lumi4.png'],
-      'statistics': [
-        {
-          'percentage': '12%',
-          'description': 'Raised capital\nfrom MassChallenge'
-        },
-        {'percentage': '', 'description': 'Business brokerage\nfinancial consulting'}
-      ],
-      'tags': ['MOBILE', 'WEB', 'Financial Solutions', 'Investment Services']
-    },
-    {
-      'id': '05',
-      'title': 'OnlyBandOption | Cryptocurrency & Investment Services Built with Flutter',
-      'images': ['assets/onad4.png', 'assets/onad7.png'],
-      'statistics': [
-        {
-          'percentage': '73%',
-          'description': 'Trust Score\nin AI guided trading'
-        },
-        {'percentage': '15%', 'description': 'Traffic ranking in\nreturning users'}
-      ],
-      'tags': ['MOBILE', 'WEB', 'Cryptocurrency','Investments', 'Flutter']
-    },
-    {
-      'id': '06',
-      'title': 'Investment Cove built with Flutter',
-      'images': ['assets/cove2.png', 'assets/cove6.png'],
-      'statistics': [
-        {
-          'percentage': '8%',
-          'description': 'Foreign Direct Investment\nDecline in 2024'
-        },
-        {'percentage': '5%', 'description': 'Increase in\nMutual funds \nand ETFs'}
-      ],
-      'tags': ['MOBILE', 'WEB', 'Investments', 'Flutter']
-    },
-    {
-      'id': '07',
-      'title': 'SmartCRM - comprehensive CRM system',
-      'images': ['assets/crm2.jpg', 'assets/crm6.jpg'],
-      'statistics': [
-        {
-          'percentage': '42%',
-          'description': 'Increase in\nSales Management'
-        },
-        {'percentage': '85%', 'description': 'Increase in\nInsights and Analytics'}
-      ],
-      'tags': ['ERP Integration', 'WEB', 'Data Analytics', 'SQL', 'Local Server', 'ReactJS']
-    },
-    {
-      'id': '08',
-      'title': 'Empowered 500k Users with AI-Enhanced Resume Builder',
-      'images': ['assets/asset1.jpg', 'assets/asset2.jpg'],
-      'statistics': [
-        {
-          'percentage': '12%',
-          'description': 'Click rates for\nJob Description and AI writer'
-        },
-        {'percentage': '5%', 'description': 'Increase in\nresume building'}
-      ],
-      'tags': ['MOBILE', 'WEB', '500K MAU']
-    },
-    {
-      'id': '09',
-      'title': 'Empowered 500k Users with AI-Enhanced Resume Builder',
-      'images': ['assets/asset1.jpg', 'assets/asset2.jpg'],
-      'statistics': [
-        {
-          'percentage': '12%',
-          'description': 'Click rates for\nJob Description and AI writer'
-        },
-        {'percentage': '5%', 'description': 'Increase in\nresume building'}
-      ],
-      'tags': ['MOBILE', 'WEB', '500K MAU']
-    },
-    {
-      'id': '10',
-      'title': 'Empowered 500k Users with AI-Enhanced Resume Builder',
-      'images': ['assets/asset1.jpg', 'assets/asset2.jpg'],
-      'statistics': [
-        {
-          'percentage': '12%',
-          'description': 'Click rates for\nJob Description and AI writer'
-        },
-        {'percentage': '5%', 'description': 'Increase in\nresume building'}
       ],
       'tags': ['MOBILE', 'WEB', '500K MAU']
     },
